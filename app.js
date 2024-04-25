f827954c8a39128ee270646b470f92850da10455
KABANOFESTO <kabanofesto1@gmail.com>
2024-04-25 12:46:21 +0200

    handle-error

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const usersRoutes = require('./src/routes/userRouter');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use('/users', usersRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
