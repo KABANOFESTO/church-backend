ff67182abfa072cd1f21f7b3ed4c200cfeb9ddae
KABANOFESTO <kabanofesto1@gmail.com>
2024-04-29 14:05:36 +0200

    ch-deleteBlog

--- src/routes/blogRouter.js
+++ src/routes/blogRouter.js
@@ -87,4 +87,91 @@ blogRouter.patch('/update', async (req, res) => {
 
 })
 
+blogRouter.delete('/delete', (req, res) => {
+    const { id } = req.params
+    User.findOne({
+        _id: req.user.id
+    }).then((user) => {
+        if (user.role.toString() == 'admin') {
+            Blog.deleteOne({ _id: id })
+                .then(result => {
+                    res.status(200).json(result)
+                })
+                .catch(error => {
+                    res.status(404).json({ error: 'blog douesnt exist' })
+                })
+
+        } else {
+            res.status(401).json({ message: 'User Not Authorized' })
+        }
+    }).catch((error) => {
+        res.status(500).json({ message: error })
+    })
+})
+
+blogRouter.post('/comment/:blog_id', (req, res) => {
+    const { blog_id } = req.params
+    const { comment } = req.body
+    User.findOne({
+        _id: req.user.id
+    }).then((user) => {
+        const newComment = {
+            user_id: user._id,
+            username: user.username,
+            comment,
+            postedDate: today
+        }
+        Blog.findOne({ _id: blog_id })
+            .then(blog => {
+                if (blog) {
+                    blog.comments.push(newComment)
+                    blog.save()
+                        .then(result => res.status(201).json(result))
+                        .catch(error => res.status(500).json({ error: error.message }))
+                } else {
+                    res.status(404).json({ error: "blog doesn't exist" })
+                }
+            })
+            .catch(error => res.status(500).json(console.log(error)))
+    })
+})
+
+blogRouter.post('/likes/:blog_id', (req, res) => {
+    const { blod_id } = req.params
+    const user_id = req.user.id
+    const newLike = {
+        user_id,
+    }
+    Blog.findOne({ _id: blod_id })
+        .then(blog => {
+            if (blog) {
+                const found = blog.likes.some(el => el.user_id.toString() === user_id.toString());
+                if (found) {
+                    blog.likes = blog.likes.filter(item => item.user_id.toString() !== user_id.toString())
+                } else {
+                    blog.likes.push(newLike);
+                }
+                blog.save()
+                    .then(result => res.status(201).json(result))
+                    .catch(error => res.status(500).json({ error: error.message }))
+            }
+            else {
+                res.status(404).json({ error: "blog doesn't exist" })
+            }
+        }).catch(error => res.status(500).json({ error: error.message }))
+})
+blogRouter.get('/single', (req, res) => {
+    const { id } = req.params
+    Blog.findOne({ _id: id })
+        .then(result => {
+            if (result)
+                res.status.json(result)
+            else
+                res.status(404).json('You are not Autorized')
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message })
+        })
+})
+
 module.exports = blogRouter;
