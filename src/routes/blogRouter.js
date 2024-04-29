2d9e2d4460bb9e3ba50dc6c102a10bac661b24e3
KABANOFESTO <kabanofesto1@gmail.com>
2024-04-29 11:02:21 +0200

    ch-getBlogBydate

--- src/routes/blogRouter.js
+++ src/routes/blogRouter.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const blogRouter = express();
 const Blog = require('../models/blog');
+const User = require('../models/userModel');
 const { blogSchema, updateArticleSchema } = require('../help/validation');
-
 const imageUpload = require('../help/photoupload');
 
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0");
+var yyyy = today.getFullYear();
+
+today = dd + "/" + mm + "/" + yyyy
 
 
-blogRouter.post('/');
+blogRouter.get('/all', async (req, res) => {
+    Blog.find()
+        .then(result => {
+            res.status(200).json(result)
+        })
+});
 
 module.exports = blogRouter;
